9d07ab4efc715819ba9af162a45955af1118b2f1
Adam Sandberg Ericsson <adam@sandbergericsson.se>
2020-10-06 19:51:31 +0100

    improve error message for `FromField UTCTime` and `FromField Day`

--- Database/SQLite/Simple/FromField.hs
+++ Database/SQLite/Simple/FromField.hs
@@ -181,7 +181,7 @@ instance FromField UTCTime where
   fromField f@(Field (SQLText t) _) =
     case parseUTCTime t of
       Right t -> Ok t
-      Left e -> returnError ConversionFailed f ("couldn't parse UTCTime field: " ++ e)
+      Left e -> returnError ConversionFailed f ("couldn't parse UTCTime field: " ++ e ++ ", field contents: " ++ T.unpack t)
 
   fromField f = returnError ConversionFailed f "expecting SQLText column type"
 
@@ -190,7 +190,7 @@ instance FromField Day where
   fromField f@(Field (SQLText t) _) =
     case parseDay t of
       Right t -> Ok t
-      Left e -> returnError ConversionFailed f ("couldn't parse Day field: " ++ e)
+      Left e -> returnError ConversionFailed f ("couldn't parse Day field: " ++ e ++ ", field contents: " ++ T.unpack t)
 
   fromField f = returnError ConversionFailed f "expecting SQLText column type"
 
